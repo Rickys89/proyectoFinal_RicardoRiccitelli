5467b17874a384867444dbdf1c8706c1372d72db
Rickys89 <r.riccitelli89@gmail.com>
2022-08-18 20:06:41 -0300

    Orden de carpetas Front y Back

---
+++ proyectoFinal_RicardoRiccitelli/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APlogoComponent } from './components/aplogo/aplogo.component';
+import { ffondopantallaComponent } from './components/ffondopantalla/ffondopantalla.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    APlogoComponent,
+    ffondopantallaComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    FooterComponent,
+    ProyectoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
